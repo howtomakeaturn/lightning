ed48e6d933df8fdcfbaef1b6e70a558beffdc682
chuan-hao, you <howtomakeaturn@hotmail.com>
2014-04-15 10:11:13 +0800

    Admin panel tav-tabs

--- application/views/layouts/admin.php
+++ application/views/layouts/admin.php
@@ -1,14 +1,48 @@
 <!DOCTYPE html>
 <html>
     <head>
+        <meta charset='utf8' />
         <title><?php echo $template['title']; ?></title>
         <?php echo $template['metadata']; ?>
         <link rel="stylesheet" href="/assets/css/font-awesome.min.css">
         <link rel="stylesheet" type="text/css" media="screen" href="/assets/css/bootstrap.min.css">
         <script type="text/javascript" src="/assets/js/jquery-1.10.2.min.js"></script> 
         <script type="text/javascript" src="/assets/js/bootstrap.min.js"></script>      
+        <style>
+            .nav-tabs{
+                font-size: 36px;
+            }
+            
+            @media (min-width:1200px) { 
+                .nav-tabs li a{
+                    padding-left: 40px;
+                    padding-right: 40px;              
+                } 
+            }
+        </style>
     </head>
     <body>
-        <?php echo $template['body']; ?>
+        <div class="container">
+            <div class="navbar-header">
+                <ul class="nav nav-tabs">
+                  <li class="active"><a href="#">管理</a></li>
+                  <li><a href="#">新聞</a></li>
+                  <li><a href="#">頁面</a></li>
+                  <li><a href="#">產品</a></li>
+                  <li><a href="#">順序</a></li>
+                  <li><a href="#">美觀</a></li>
+                  <li><a href="#">支援</a></li>
+                </ul>
+            </div>
+        </div>
+        <div class="container">
+
+            <!-- Main component for a primary marketing message or call to action -->
+            <div>
+                <?php echo $template['body']; ?>
+            </div>
+        </div> <!-- /container -->
+      
+      
     </body>
 </html>
