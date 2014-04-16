827666e5347b521550e52aa349a0508ddbb4234d
chuan-hao, you <howtomakeaturn@hotmail.com>
2014-04-16 23:06:06 +0800

    Add common layout and index

--- application/views/common/index.php
+++ application/views/common/index.php
@@ -1,1 +1,52 @@
-<h1>THIS IS COMMON</h1>
+
+<div id="carousel-example-generic" class="carousel slide" data-ride="carousel">
+  <!-- Indicators -->
+  <ol class="carousel-indicators">
+    <li data-target="#carousel-example-generic" data-slide-to="0" class="active"></li>
+    <li data-target="#carousel-example-generic" data-slide-to="1"></li>
+    <li data-target="#carousel-example-generic" data-slide-to="2"></li>
+    <li data-target="#carousel-example-generic" data-slide-to="3"></li>
+  </ol>
+
+  <!-- Wrapper for slides -->
+  <div class="carousel-inner">
+
+    <div class="item active">
+      <img src="/assets/img/mock/banner1.jpg">
+      <div class="carousel-caption">
+
+      </div>
+    </div>
+
+    <div class="item">
+      <img src="/assets/img/mock/banner2.jpg">
+      <div class="carousel-caption">
+
+      </div>
+    </div>
+
+    <div class="item">
+      <img src="/assets/img/mock/banner3.jpg">
+      <div class="carousel-caption">
+
+      </div>
+    </div>
+
+    <div class="item">
+      <img src="/assets/img/mock/banner4.jpg">
+      <div class="carousel-caption">
+
+      </div>
+    </div>
+
+
+  </div>
+
+  <!-- Controls -->
+  <a class="left carousel-control" href="#carousel-example-generic" data-slide="prev">
+    <span class="glyphicon glyphicon-chevron-left"></span>
+  </a>
+  <a class="right carousel-control" href="#carousel-example-generic" data-slide="next">
+    <span class="glyphicon glyphicon-chevron-right"></span>
+  </a>
+</div>
--- application/views/layouts/public.php
+++ application/views/layouts/public.php
@@ -7,9 +7,63 @@
 		<link rel="stylesheet" type="text/css" media="screen" href="/assets/css/bootstrap.min.css">
     <script type="text/javascript" src="/assets/js/jquery-1.10.2.min.js"></script> 
     <script type="text/javascript" src="/assets/js/bootstrap.min.js"></script>      
+    <style>
+            #background {
+                width: 100%; 
+                height: 100%; 
+                position: fixed; 
+                left: 0px; 
+                top: 0px; 
+                z-index: -1; /* Ensure div tag stays behind content; -999 might work, too. */
+            }
+            .stretch {
+                width:100%;
+                height:100%;
+            }
+
+            .navbar{
+                margin-top: 40px;
+            }
+            
+    </style>
 	</head>
 	<body>
-		<h1><?php echo $template['title']; ?></h1>
-		<?php echo $template['body']; ?>
+      <div id="background">
+          <img src="/assets/img/background/Elegant_Background-2.jpg" class="stretch" alt="" />
+      </div>    
+      <div class='container'>
+          <header>
+<!--              <img class='ln-logo' src='/assets/img/mock/logo.png' /> -->
+              <nav class="navbar navbar-inverse" role="navigation">
+                <div class="container-fluid">
+                  <!-- Brand and toggle get grouped for better mobile display -->
+                  <div class="navbar-header">
+                    <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+                      <span class="sr-only">Toggle navigation</span>
+                      <span class="icon-bar"></span>
+                      <span class="icon-bar"></span>
+                      <span class="icon-bar"></span>
+                    </button>
+                    <a class="navbar-brand" href="#">Brand</a>
+                  </div>
+
+                  <!-- Collect the nav links, forms, and other content for toggling -->
+                  <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+                    <ul class="nav navbar-nav">
+                      <li><a href="/common/index">Home</a></li>
+                      <li><a href="#">Blog</a></li>
+                      <li><a href="#">Menu</a></li>
+                      <li><a href="#">Map</a></li>
+                      <li><a href="#">Contact Us</a></li>
+
+                    </ul>
+                  </div><!-- /.navbar-collapse -->
+                </div><!-- /.container-fluid -->
+              </nav>
+          </header>
+      </div>
+      <div class='container'>
+      		<?php echo $template['body']; ?>
+      </div>
 	</body>
 </html>
