911940d9f65806765d2ef8690feff8aba200dd3d
chuan-hao, you <howtomakeaturn@hotmail.com>
2014-04-22 17:01:02 +0800

    1. Add SimpleLoginSecure library 2. Add company intro template

---
+++ application/controllers/intro.php
@@ -0,0 +1,10 @@
+<?php
+class Intro extends CI_Controller{
+    function __construct(){
+        parent::__construct();
+        $this->template->set_layout('intro');
+    }
+    function index(){
+        $this->template->build('intro/index');
+    }
+}
