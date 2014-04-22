676dfc2dab31815308178dd4096af52fad9eb7e8
chuan-hao, you <howtomakeaturn@hotmail.com>
2014-04-23 07:14:51 +0800

    Set theme based on subdomain

--- application/controllers/common.php
+++ application/controllers/common.php
@@ -4,8 +4,18 @@ class Common extends Public_Controller
     function __construct()
     {
         parent::__construct();
-        // Whatever
-#        $this->data['stuff'] = $whatever;
+
+        $url_host = $_SERVER['HTTP_HOST'];
+        $url_segments = explode('.', $url_host);
+        $subdomain = $url_segments[0];
+        $this->load->database();
+        
+        // I think the codes can be further improved by JOIN statements
+        $site = $this->db->from('sites')->where('subdomain', $subdomain)->get()->row();
+        $layout = $this->db->from('layouts')->where('site_id', $site->id)->get()->row();
+        $theme = $this->db->from('themes')->where('id', $layout->theme_id)->get()->row();
+        
+        $this->template->set_theme($theme->name);
     }
     
     function index(){
