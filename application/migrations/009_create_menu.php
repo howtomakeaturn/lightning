170d1eb79040e499014bb4ad6342c7c31a6440f1
chuan-hao, you <howtomakeaturn@hotmail.com>
2014-04-29 07:40:00 +0800

    Add menu migration file

---
+++ application/migrations/009_create_menu.php
@@ -0,0 +1,51 @@
+<?php
+
+class Migration_Create_menu extends CI_Migration {
+    public function up(){
+        $this->dbforge->add_field(array(
+            'id' => array(
+              'type' => 'INT',
+              'constraint' => '10',
+              'unsigned' => TRUE,
+              'auto_increment' => TRUE
+            ),
+            'site_id' => array(
+              'type' => 'INT',
+              'constraint' => '10',
+              'unsigned' => TRUE,
+            ),
+            'name' => array(
+              'type' => 'VARCHAR',
+              'constraint' => '32'
+            )
+        ));
+        $this->dbforge->add_key('id', TRUE);
+        $this->dbforge->create_table('menu_categories');
+
+        $this->dbforge->add_field(array(
+            'id' => array(
+              'type' => 'INT',
+              'constraint' => '10',
+              'unsigned' => TRUE,
+              'auto_increment' => TRUE
+            ),
+            'category_id' => array(
+              'type' => 'INT',
+              'constraint' => '10',
+              'unsigned' => TRUE,
+            ),
+            'name' => array(
+              'type' => 'VARCHAR',
+              'constraint' => '32'
+            )
+        ));
+        $this->dbforge->add_key('id', TRUE);
+        $this->dbforge->create_table('menu_items');
+
+    }
+
+    public function down(){
+        $this->dbforge->drop_table('menu_categories');
+        $this->dbforge->drop_table('menu_items');
+    }
+}
