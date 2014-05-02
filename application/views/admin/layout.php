659af0f6f114b65907f0cab708fd4d0cfcb3057e
chuan-hao, you <howtomakeaturn@hotmail.com>
2014-05-02 23:53:22 +0800

    1. Only show upload button while no logo 2. Change text

--- application/views/admin/layout.php
+++ application/views/admin/layout.php
@@ -10,11 +10,14 @@
 
 </style>
 
-<h3>您的公司有LOGO圖片嗎？</h3>
-<form method='post' action='/admin/upload_logo' enctype="multipart/form-data">
-    <input type='file' name='userfile' class='form-control' />
-    <input type='submit' class='btn btn-success' value='上傳LOGO' onclick='return confirm("確定上傳LOGO？")' />
-</form>
+<h3>公司LOGO圖片</h3>
+
+<?php if (empty($logos)): ?>
+    <form method='post' action='/admin/upload_logo' enctype="multipart/form-data">
+        <input type='file' name='userfile' class='form-control' />
+        <input type='submit' class='btn btn-success' value='上傳LOGO' onclick='return confirm("確定上傳LOGO？")' />
+    </form>
+<?php endif; ?>
 
 <table class='table table-hover table-bordered'>
     <caption>目前LOGO</caption>
@@ -34,7 +37,7 @@
 <?php endforeach; ?>
 </table>
 
-<h3>放幾張圖片到首頁嗎？</h3>
+<h3>首頁圖片</h3>
 <span class="help-block">例如：產品照片、公司照片、老闆照片、工廠照片，讓客戶更信任您。</span>
 <form method='post' action='/admin/upload_banner' enctype="multipart/form-data">
     <input type='file' name='userfile' class='form-control' />
